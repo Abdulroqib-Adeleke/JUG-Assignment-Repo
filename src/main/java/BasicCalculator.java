979976568c184a48da25043b02b8561915854d95
Abdulroqib Adeleke <162007856+Abdulroqib-Adeleke@users.noreply.github.com>
2026-01-30 14:22:34 +0100

    First JUG assignment submission

---
+++ src/main/java/BasicCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+
+        int first = 5;
+        int second = 5;
+
+        System.out.println(first + " + " + second + " = " + (first + second));
+        System.out.println(first + " - " + second + " = " + (first - second));
+        System.out.println(first + " * " + second + " = " + (first * second));
+        System.out.println(first + " / " + second + " = " + (1.0 * first / second));
+    }
+}
