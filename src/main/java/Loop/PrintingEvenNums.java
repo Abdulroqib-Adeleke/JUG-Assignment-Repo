349cf9b40f3877825346f6f54ef7c2c2ad06ee99
Abdulroqib Adeleke <162007856+Abdulroqib-Adeleke@users.noreply.github.com>
2026-02-06 00:18:41 +0100

    Using for loop

---
+++ src/main/java/Loop/PrintingEvenNums.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class PrintingInTwo {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+
+            if(i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+}
